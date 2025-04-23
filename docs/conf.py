9a895f4181e9882dee49a8e00da2990da94eafb3
FixOut <fixout.tech@gmail.com>
2025-04-23 14:52:53 +0200

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -5,8 +5,8 @@
 
 
 import os
-#import sys
-#sys.path.insert(0, os.path.abspath('../fixout'))
+import sys
+sys.path.insert(0, os.path.abspath('fixout'))
 
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
